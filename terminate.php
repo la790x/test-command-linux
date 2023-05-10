6f62e86fccc3ce7a8cdace5f6136cfdd1ef207c1
la790x <la790x@gmail.com>
2023-05-10 10:48:43 +0700

    rev dikit

--- terminate.php
+++ terminate.php
@@ -7,10 +7,10 @@ exec("lsof -i :$port", $output, $return_value);
 
 if ($return_value === 0) {
     // port is in use
-    echo "Port $port is in use.";
+    echo "Port $port is in use." . PHP_EOL;
     // output the result of the command
-    echo implode("\n", $output);
+    echo implode("\n", $output) . PHP_EOL;
   } else {
     // port is not in use
-    echo "Port $port is not in use.";
+    echo "Port $port is not in use." . PHP_EOL;
   }
