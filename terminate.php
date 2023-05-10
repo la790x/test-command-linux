c7931f4bc10e92021ce5b2bed49ed667ce213e85
la790x <la790x@gmail.com>
2023-05-10 11:01:30 +0700

    fix dikit

--- terminate.php
+++ terminate.php
@@ -11,7 +11,7 @@ if ($return_value === 0) {
     // port is in use
     echo "Port $port is in use." . PHP_EOL;
     // output the result of the command
-    dump($port);
+    dump($return_value);
   } else {
     // port is not in use
     echo "Port $port is not in use." . PHP_EOL;
