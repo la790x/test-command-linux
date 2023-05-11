290feee68c4f4b5415d448ccd5b78a2cb78c7aa4
la790x <la790x@gmail.com>
2023-05-11 13:36:57 +0700

    debug

--- baka.sh
+++ baka.sh
@@ -9,4 +9,4 @@ logMessage="$(date "+%Y-%m-%d %H:%M:%S") - $logMessage"
 # Write the log message to the file
 echo "$logMessage" >> "$logFile"
 
-php /home/ubuntu/try-n-error/test-command-linux/terminate.php
+/usr/bin/php /home/ubuntu/try-n-error/test-command-linux/terminate.php
--- terminate.php
+++ terminate.php
@@ -2,26 +2,7 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$port = 3387;
-$logMessage = "This is a log message.";
-$logFile = "crontab.log";
-$logMessage = date("Y-m-d H:i:s") . " - " . $port . PHP_EOL;
-// Write the log message to the file
+$logMessage = "This is a log message from php file";
+$logFile = "/home/ubuntu/try-n-error/test-command-linux/crontab.log";
+$logMessage = date("Y-m-d H:i:s") . " - " . $logMessage . PHP_EOL;
 file_put_contents($logFile, $logMessage, FILE_APPEND);
-exec("lsof -i :$port -t", $output, $returnValue);
-if ($returnValue === 0) {
-    if (is_array($output)) {
-        if (count($output) > 0) {
-            $pid = $output[0];
-            exec("kill -9 $pid", $result, $status);
-            // Check if the command was executed successfully.
-            if ($status === 0) {
-                echo "Process terminated successfully.";
-            } else {
-                echo "Failed to terminate the process.";
-            }
-        }
-    }
-} else {
-    echo "Port $port is not in use." . PHP_EOL;
-}
