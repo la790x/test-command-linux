30b865562c5fad40fa8cbcb31ac870ee3182b5dc
la790x <la790x@gmail.com>
2023-05-11 13:23:41 +0700

    puck

--- baka.sh
+++ baka.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-php /home/ubuntu/try-n-error/test-command-linux/terminate.php
+logFile="/home/ubuntu/try-n-error/test-command-linux/crontab.log"
+logMessage="This is a log message."
+
+# Add timestamp to the log message
+logMessage="$(date "+%Y-%m-%d %H:%M:%S") - $logMessage"
+
+# Write the log message to the file
+echo "$logMessage" >> "$logFile"
+
+# php /home/ubuntu/try-n-error/test-command-linux/terminate.php
